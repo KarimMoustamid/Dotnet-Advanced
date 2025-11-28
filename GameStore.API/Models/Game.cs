b0918e71bf45fd52ac20b105f0ce7db1e8c6e596
KarimMoustamid <moustamid.teck@gmail.com>
2025-11-28 12:26:01 -0600

    Add Game model and sample data to API

---
+++ GameStore.API/Models/Game.cs
@@ -0,0 +1,11 @@
+namespace GameStore.API.Models
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string  Genre { get; set; }
+        public required decimal Price { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+    }
+}
--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -1,3 +1,4 @@
+using GameStore.API.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,5 +12,36 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+List<Game> games = new List<Game>
+{
+    new Game
+    {
+        Id = Guid.NewGuid(),
+        Name = "The Witcher 3: Wild Hunt",
+        Genre = "RPG",
+        Price = 39.99M,
+        ReleaseDate = new DateOnly(2015, 5, 19)
+    },
+    new Game
+    {
+        Id = Guid.NewGuid(),
+        Name = "Cyberpunk 2077",
+        Genre = "Action RPG",
+        Price = 59.99M,
+        ReleaseDate = new DateOnly(2020, 12, 10)
+    },
+    new Game
+    {
+        Id = Guid.NewGuid(),
+        Name = "Hades",
+        Genre = "Roguelike",
+        Price = 24.99M,
+        ReleaseDate = new DateOnly(2020, 9, 17)
+    }
+};
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/games", () => games);
 
 app.Run();

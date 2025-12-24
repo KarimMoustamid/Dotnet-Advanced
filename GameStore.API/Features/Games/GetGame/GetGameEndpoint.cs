1c89c13c34374b437a88c8ab6db6f9ca8cad7f98
KarimMoustamid <moustamid.teck@gmail.com>
2025-12-23 22:26:58 -0600

    Refactor GetGameEndpoint to use FindAsync directly and set ASPNETCORE_ENVIRONMENT to Development in launchSettings.json

--- GameStore.API/Features/Games/GetGame/GetGameEndpoint.cs
+++ GameStore.API/Features/Games/GetGame/GetGameEndpoint.cs
@@ -14,18 +14,11 @@ namespace GameStore.API.Features.Games.GetGame
             app.MapGet("/{id}",
                 async (Guid id, GameStoreContext dbContext, ILogger<Program> logger) =>
                 {
-                        Game? game = await FindGameAsync(dbContext, id);
+                        Game? game = await dbContext.Games.FindAsync(id);
 
                         return game is null ? Results.NotFound() : Results.Ok(new GetGameDto(
                             game.Id, game.Name, game.GenreId, game.Price, game.ReleaseDate, game.Description));
                 }).WithName(EndpointNames.GetGame);
-        }
-
-        private async static Task<Game?> FindGameAsync(GameStoreContext dbContext, Guid id)
-        {
-            throw new SqliteException("The database is not available.", 14);
-            Game? game = await dbContext.Games.FindAsync(id);
-            return game;
         }
     }
 }

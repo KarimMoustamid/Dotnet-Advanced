1904ec48665c951f484f861a36ec518053d45870
KarimMoustamid <moustamid.teck@gmail.com>
2025-12-23 20:01:08 -0600

    Add error handling for database availability in GetGameEndpoint

--- GameStore.API/Features/Games/GetGame/GetGameEndpoint.cs
+++ GameStore.API/Features/Games/GetGame/GetGameEndpoint.cs
@@ -3,6 +3,7 @@ namespace GameStore.API.Features.Games.GetGame
     using Data;
     using Dtos;
     using Features.Games.Constants;
+    using Microsoft.Data.Sqlite;
     using Models;
 
     public static class GetGameEndpoint
@@ -12,11 +13,18 @@ namespace GameStore.API.Features.Games.GetGame
             app.MapGet("/{id}",
                 async (Guid id, GameStoreContext dbContext) =>
                 {
-                    Game? game = await dbContext.Games.FindAsync(id);
+                    Game? game = await FindGameAsync(dbContext, id);
 
                     return game is null ? Results.NotFound() : Results.Ok(new GetGameDto(
                         game.Id, game.Name, game.GenreId, game.Price, game.ReleaseDate, game.Description));
                 }).WithName(EndpointNames.GetGame);
+        }
+
+        private async static Task<Game?> FindGameAsync(GameStoreContext dbContext, Guid id)
+        {
+            throw new SqliteException("The database is not available.", 14);
+            Game? game = await dbContext.Games.FindAsync(id);
+            return game;
         }
     }
 }
